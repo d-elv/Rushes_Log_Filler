56f447f9cd451eb113091351b641ea47863f7b25
Daniel Elvey <delvey21@hotmail.co.uk>
2019-12-29 12:44:58 +0000

    Gets all info for one roll. This works as a function that calls two other functions

--- Rushes_Log_Filler.py
+++ Rushes_Log_Filler.py
@@ -1,5 +1,6 @@
 import os, datetime, pyperclip
 
+
 def getFolderSize(folder):
     total_size = os.path.getsize(folder)
     for item in os.listdir(folder):
@@ -10,6 +11,7 @@ def getFolderSize(folder):
             total_size += getFolderSize(itempath)
     return total_size
 
+
 def getFileCount(folder):
     fileCount = 1
     for item in os.listdir(folder):
@@ -18,49 +20,57 @@ def getFileCount(folder):
             fileCount += 1
     return fileCount
 
-directoryToScan = input('Please drag the folder containing the rolls that require logging: ')
 
-fileListinRoll = []
-rollList = []
-dirsList = []
-rollSpecifier = ''
-rollDirList = []
-rootSplit = []
-dirsSplit = []
-firstFile = ''
-lastFile = ''
+# directoryToScan = input('Please drag the folder containing the rolls that require logging: ')
+directoryToScan = input('Please drag in the roll that requires logging: ')
 
-rejectFiles = ['.DS_Store', 'Thumbs.db']
-acceptedFiles = ['.mxf', '.mov', '.mp4', '.avi', '.vob', '.wav', '.aif', '.mp3', '.png', '.tif', '.jpg']
 
-for root, path, file in os.walk(directoryToScan):
-    rollDirList.append(root)
-    for f in file:
-        file_path = os.path.join(root + os.sep + f)
-        name, ext = os.path.splitext(f)
-        ext = ext.lower()
-        if ext in acceptedFiles:
-            fileListinRoll.append(f)
-            fileListinRoll.sort()
-            # Finds the first file and the last file
-            firstFile = fileListinRoll[0]
-            lastFile = fileListinRoll[-1]
+def rollInfoGetter(directoryToScan):
+    fileListinRoll = []
+    rollDirList = []
+    firstFile = ''
+    lastFile = ''
 
-# Finds the roll name
-currentRoll = rollDirList[0]
-currentRoll = currentRoll.split('/')
-currentRoll = currentRoll[-1]
+    rejectFiles = ['.DS_Store', 'Thumbs.db']
+    acceptedFiles = ['.mxf', '.mov', '.mp4', '.avi', '.vob', '.wav', '.aif', '.mp3', '.png', '.tif', '.jpg']
 
-# Gives you the size of the roll
-dirSize = getFolderSize(directoryToScan)
-dirSize = dirSize / 1000000
+    for root, path, file in os.walk(directoryToScan):
+        rollDirList.append(root)
+        for f in file:
+            file_path = os.path.join(root + os.sep + f)
+            name, ext = os.path.splitext(f)
+            ext = ext.lower()
+            if ext in acceptedFiles:
+                fileListinRoll.append(f)
+                fileListinRoll.sort()
+                # Finds the first file and the last file
+                firstFile = fileListinRoll[0]
+                lastFile = fileListinRoll[-1]
 
-fileCount = getFileCount(directoryToScan)
+    # Finds the roll name
+    currentRoll = rollDirList[0]
+    currentRoll = currentRoll.split('/')
+    currentRoll = currentRoll[-1]
 
-# Gives you the output of one roll, ready to paste into an excel doc
-print(currentRoll + '\t' + str(dirSize) + '\t' + str(fileCount) + '\t' + firstFile + '\t' + lastFile )
+    # Gives you the size of the roll
+    dirSize = getFolderSize(directoryToScan)
+    dirSize = dirSize / 1000000
 
-# Test Commit
+    fileCount = getFileCount(directoryToScan)
+
+    # Gives you the output of one roll, ready to paste into an excel doc
+    print(currentRoll + '\t' + str(dirSize) + '\t' + str(fileCount) + '\t' + firstFile + '\t' + lastFile )
+
+
+rollInfoGetter(directoryToScan)
+
+
+
+
+
+
+
+# Test Commit 2
 
 # for dirs in os.listdir(directoryToScan):
 #     if dirs not in rejectFiles:
